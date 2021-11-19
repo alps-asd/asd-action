8f00aebc3a57f49b05066d4e09d8dc83cb9b1ada
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-11-19 16:20:41 +0900

    Change workflow name

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ composer require koriym/app-state-diagram
 dirname=$(dirname $2)
 ./vendor/bin/asd --config=$1 $2
 echo "Output Directory: $dirname"
-echo "::set-output name=latest_output_dir::$dirname"
+echo "::set-output name=dir::$dirname"
